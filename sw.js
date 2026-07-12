7e1cdd80d4dc63e470f0a30a45194cd430db44cc
gene10969 <onokrk@gmail.com>
2026-07-13 03:43:07 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gene-check-obfuscated-level2-v20260713-json-filename-fix';
+const CACHE_NAME = 'gene-check-obfuscated-level2-v20260713-checkdate-ui-fix';
 const ASSETS = [
   './',
   './index.html',
